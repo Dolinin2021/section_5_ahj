da97826bd0590345a85506ad4488a5990c47a491
Dolinin2021 <programmistdolinin@mail.ru>
2023-06-28 20:12:42 +0600

    fix

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,14 +1,13 @@
 import puppetteer from "puppeteer";
 import { fork } from "child_process";
 
-jest.setTimeout(20000);
+jest.setTimeout(40000);
 describe("Popovers", () => {
   let browser = null;
   let page = null;
   let server = null;
-  const baseUrl = "http://localhost:9000";
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     server = fork(`${__dirname}/e2e.server.js`);
     await new Promise((resolve, reject) => {
       server.on("error", reject);
@@ -20,19 +19,27 @@ describe("Popovers", () => {
     });
 
     browser = await puppetteer.launch({
-      // headless: false, // show gui
-      // slowMo: 250,
-      // devtools: true, // show devTools
+      headless: false, // show gui
+      slowMo: 250,
+      devtools: true, // show devTools
     });
     page = await browser.newPage();
+  });
+
+  test("button click", async () => {
+    try {
+      await page.goto("http://localhost:9000");
+      const button = await page.$(".button-tooltip ");
+      await button.click();
+      await page.waitForSelector(".container");
+    } catch (err) {
+      console.log(err.message);
+      return false;
+    }
   });
 
   afterAll(async () => {
     await browser.close();
     server.kill();
-  });
-
-  test("should add do something", async () => {
-    await page.goto(baseUrl);
-  });
+  }, 100000);
 });
